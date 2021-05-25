537c94184c4b6ae5175ca57286e74bfcfa62c466
zax5021 <zacharyacampbell1@gmail.com>
2021-05-25 01:49:13 -0700

    added searchForm

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,4 +1,27 @@
 function SearchForm() {
-  return <div>SearchForm</div>;
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h2 className="card-title">Book Search</h2>
+        <form className="">
+          <div className="form-group d-flex flex-column">
+            <input
+              className="form-control mt-4"
+              type="text"
+              placeholder="Enter Book Here"
+              aria-label="Search"
+              id="search"
+            />
+            <button
+              className="btn btn-outline-primary mt-2 ml-auto px-4 "
+              type="submit"
+            >
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 export default SearchForm;
